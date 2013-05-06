cec8610cf8145189c08837a2d0e8082e9b1fc83d
Alexey <appplemac@gmail.com>
2013-05-06 21:20:42 +0200

    Stuff

--- jobs/jobs.R
+++ jobs/jobs.R
@@ -1,13 +1,16 @@
-jobs_data = read.csv("jobs_365d.csv", header=TRUE)
-jobs <- c(jobs_data$active_jobs)
-time <- strptime(jobs_data$readable_time, "%Y/%m/%d %H:%M:%S")
+jobs_data_year = read.csv("jobs_365d.csv", header=TRUE)
+jobs_data_month = read.csv("jobs_30d.csv", header=TRUE)
+jobs_month <- c(jobs_data_month$active_jobs)
+jobs_year <- c(jobs_data_year$active_jobs)
 
-mean(jobs)
-sd(jobs)
-xbar = mean(jobs)
-mu0 = 50
-sigma = sd(jobs)
-n = length(jobs)
+time <- strptime(jobs_data_month$readable_time, "%Y/%m/%d %H:%M:%S")
+
+mean(jobs_month)
+sd(jobs_month)
+xbar = mean(jobs_month)
+mu0 = mean(jobs_year)
+sigma = sd(jobs_month)
+n = length(jobs_month)
 z = (xbar - mu0)/(sigma/sqrt(n))
 z
 
@@ -16,4 +19,7 @@ z.half.alpha = qnorm(1-alpha/2)
 z.region = c(-z.half.alpha, z.half.alpha)
 z.region
 
-t.test(x=jobs, conf.level=0.95)
+testing = t.test(x=jobs_month, conf.level=0.95, alternative="two.sided", mu=mu0)
+testing
+# plot(testing)
+# not working
